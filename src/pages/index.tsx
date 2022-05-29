0decd3bda8955cfc5070056f41d6b3fe83e09b05
netbriler <netbriler@gmail.com>
2022-05-29 21:19:54 +0300

    Bookshelves tabs adaptivity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React, {useContext, useEffect, useState} from "react";
 import Bookshelf from "../components/bookshelf";
 import SearchBooks from "../components/search-books";
 import Sidebar from "../components/sidebar";
+import {useIsMobile} from "../hooks/useMediaQuery";
 import DefaultLayout from "../layouts/default"
 import {selectAuth} from "../store/reducers/authSlice";
 import {useAppSelector} from "../store/store";
@@ -12,6 +13,9 @@ import {SearchContext} from "./_app";
 const Index = () => {
     const {user} = useAppSelector(selectAuth);
     const bookshelves = user !== null ? user.bookshelves : [];
+
+    const isMobile = useIsMobile();
+    console.log(isMobile)
 
     const [selectedBookshelf, setSelectedBookshelf] = useState<IBookshelf | null>(null)
     const [enableSearch, setEnableSearch] = useState<boolean>(false)
@@ -36,12 +40,12 @@ const Index = () => {
 
     return (
         <DefaultLayout>
-            <Grid.Container gap={2}>
-                <Grid xs={2}>
+            <Grid.Container gap={2} direction={isMobile ? 'column' : 'row'}>
+                <Grid xs={isMobile ? 12 : 3} sm={3} md={2}>
                     <Sidebar selectedTab={selectedBookshelf ? selectedBookshelf.id : -1} tabs={bookshelves}
                              setTab={onSetTab}/>
                 </Grid>
-                <Grid xs={10} direction={'column'}>
+                <Grid xs={isMobile ? 12 : 9} sm={9} md={10} direction={'column'}>
                     {selectedBookshelf && !enableSearch ?
                         <Bookshelf bookshelf={selectedBookshelf}/> :
                         selectedBookshelf && <SearchBooks value={SearchValue.value}/>
