bc62e9e28f2ac89cee4456cc0470f36b7b736606
netbriler <netbriler@gmail.com>
2022-10-06 11:11:18 +0200

    First landing screen

--- src/layouts/public.tsx
+++ src/layouts/public.tsx
@@ -17,6 +17,9 @@ const PublicLayout: FC<PublicLayoutProps> = ({children, title}) => {
             <Head>
                 <title>{title || 'My Books History'}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
             </Head>
             <CustomNavbar isDark={isDark} showLinks={true}/>
             <Container lg={true}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import {useRouter} from "next/router";
 import React from "react";
 import PublicLayout from "../layouts/public";
 import {userAPI} from "../services/UserService";
+import {Button, Grid, Text} from "@nextui-org/react";
+import LandingImage from "../../public/landing-image.svg";
+import styles from "../styles/Index.module.css";
+import cn from "classnames";
+import {submitHandler} from "../components/auth-modal";
+
 
 const Index = () => {
     const router = useRouter()
@@ -14,7 +20,18 @@ const Index = () => {
 
     return (
         <PublicLayout>
-            Main page
+            <div className={cn(styles.container)}>
+                <div className={styles.row}>
+                    <Text h1 className={styles.title}>Let’s make your<br/><span>reading</span> enjoyable.</Text>
+                    <Text className={styles.description}>Search books via google books api and add them to your bookshelves synchronized with google library.</Text>
+                    <Button color="gradient" auto css={{marginTop: 30}} onClick={submitHandler}>
+                        Continue with Google
+                    </Button>
+                </div>
+                <div className={styles.row_image}>
+                    <LandingImage css={{width: 1000}}/>
+                </div>
+            </div>
         </PublicLayout>
     );
 };
