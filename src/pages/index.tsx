6d857bf4a9e3345511ee6fec19c4576fa16e0f7e
netbriler <netbriler@gmail.com>
2022-10-16 13:29:23 +0200

    Landing first screen dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,11 @@ import {useRouter} from "next/router";
 import React from "react";
 import PublicLayout from "../layouts/public";
 import {userAPI} from "../services/UserService";
-import {Button, Grid, Text} from "@nextui-org/react";
+import {Button, Grid, Text, useTheme} from "@nextui-org/react";
 import LandingImage from "../../public/landing-image.svg";
+import LandingImageDark from "../../public/landing-image-dark.svg";
+import ContinueWithGoogle from "../../public/continue-with-google.svg";
+import ContinueWithGoogleDark from "../../public/continue-with-google-dark.svg";
 import styles from "../styles/Index.module.css";
 import cn from "classnames";
 import {submitHandler} from "../components/auth-modal";
@@ -18,18 +21,31 @@ const Index = () => {
         router.push('dashboard')
     }
 
+    const {isDark} = useTheme();
+
     return (
         <PublicLayout>
             <div className={cn(styles.container)}>
                 <div className={styles.row}>
                     <Text h1 className={styles.title}>Let’s make your<br/><span>reading</span> enjoyable.</Text>
-                    <Text className={styles.description}>Search books via google books api and add them to your bookshelves synchronized with google library.</Text>
-                    <Button color="gradient" auto css={{marginTop: 30}} onClick={submitHandler}>
-                        Continue with Google
+                    <Text className={styles.description}
+                          css={{color: isDark ? 'rgba(255, 255, 255, .6)' : 'rgba(0, 0 , 0, .6)'}}>Search books via
+                        google books api and add them to your
+                        bookshelves synchronized with google library.</Text>
+                    <Button auto css={{
+                        marginTop: 32,
+                        backgroundColor: isDark ? '#fff' : '#000',
+                        color: isDark ? '#000' : '#fff',
+                        padding: '16px 32px',
+                        height: 48
+                    }} onClick={submitHandler}>
+                        {isDark ? <ContinueWithGoogleDark css={{height: 16}}/> :
+                            <ContinueWithGoogle css={{height: 16}}/>}
                     </Button>
                 </div>
                 <div className={styles.row_image}>
-                    <LandingImage css={{width: 1000}}/>
+                    {isDark ? <LandingImageDark css={{width: 1000, height: '100%', maxHeight: 730}}/> :
+                        <LandingImage css={{width: 1000, height: '100%', maxHeight: 730}}/>}
                 </div>
             </div>
         </PublicLayout>
